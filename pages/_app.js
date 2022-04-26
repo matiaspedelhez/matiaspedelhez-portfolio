91cf8e6d2ede78c0c818fde3336b1ac7f1e9626a
Matias Pedelhez <matiaspedelhezinf@gmail.com>
2022-04-26 05:53:12 -0300

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
